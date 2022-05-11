1e62d18df16c86344e190b0b6c5f50462c0514b8
AndreaCastel <marion.chateau@sncf.fr>
2022-05-11 14:13:22 +0200

    animation h1

--- Projet1.js
+++ Projet1.js
@@ -1,4 +1,24 @@
+const txtAnim = document.querySelector('h1');
 
+new Typewriter(txtAnim,{
+    loop: true
+    deleteSpeed: 20
+})
+
+.changeDelay(20)
+.typeString('Bienvenue en')
+.pauseFor(300)
+.typeString('<strong> Meuse</strong>')
+.pause(1000)
+.deleteChars(13)
+.typeString(<span style="color: blue">!!!</span>)
+.pauseFor(1000)
+.deleteChars(5)
+.typeString(<span style="color: red">!!!</span>)
+.pause(1000)
+.deleteChars(13)
+.typeString(<span style="color: orange">!!!</span>)
+.start()
 
 
 
