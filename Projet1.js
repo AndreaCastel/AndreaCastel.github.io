02172b044440592caadd9bb419e17effdf3393ff
AndreaCastel <marion.chateau@sncf.fr>
2022-05-11 15:31:57 +0200

    gitignore + evolution

--- Projet1.js
+++ Projet1.js
@@ -1,26 +1,27 @@
+import Typewriter from 'typewriter-effect/dist/core';
+
 const txtAnim = document.querySelector('h1');
 
-new Typewriter(txtAnim,{
-    loop: true
-    deleteSpeed: 20
-})
+new Typewriter = new Typewriter(txtAnim,{
+    deleteSpeed: 20,
+});
 
+Typerwriter
 .changeDelay(20)
 .typeString('Bienvenue en')
 .pauseFor(300)
 .typeString('<strong> Meuse</strong>')
 .pause(1000)
-.deleteChars(13)
+.deleteChars(3)
 .typeString(<span style="color: blue">!!!</span>)
 .pauseFor(1000)
 .deleteChars(5)
 .typeString(<span style="color: red">!!!</span>)
 .pause(1000)
-.deleteChars(13)
+.deleteChars(3)
 .typeString(<span style="color: orange">!!!</span>)
 .start()
-
-
+;
 
 
 
