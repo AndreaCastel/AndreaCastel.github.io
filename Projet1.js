b9c5e13a53a65986d399b72bb37298ae64f1aa34
AndreaCastel <marion.chateau@sncf.fr>
2022-05-16 11:37:33 +0200

    typewriter

--- Projet1.js
+++ Projet1.js
@@ -1,31 +1,4 @@
 
-//const Typewriter = require("typewriter-effect/dist/core");
-import Typewriter from 'typewriter-effect/dist/core';
-let txtAnim = document.getElementById('bienvenue');
-
-let typewriter = new Typewriter(txtAnim, {
-    loop: true,
-    delay: 75,
-});
-
-typewriter
-.pauseFor(1000)
-.typeString('Bienvenue en')
-.pauseFor(300)
-.typeString('<strong> Meuse</strong>')
-.pause(1000)
-.deleteChars(3)
-.typeString('<span style="color: blue">!!!</span>')
-.pauseFor(1000)
-.deleteChars(5)
-.pause(1000)
-.deleteChars(3)
-.typeString('<span style="color: orange">!!!</span>')
-.start();
-*/
-
-
-
 /*MAISON SLIDE */
 
 let slideIndex = 1;
